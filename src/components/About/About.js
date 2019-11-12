2852d05acc0638fbbc99b59292b343241bf39c8e
Devan Skeem <devanskeem@gmail.com>
2019-11-12 14:40:18 -0700

    about page updates

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -7,17 +7,10 @@ function About() {
                 <h1>About Us</h1>
                 <span className="person">
                     <h4>Michelle Pinkston, Secretary</h4>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil vitae repellendus ratione omnis, rerum error inventore, nulla assumenda itaque, illum voluptas quibusdam magni fugiat. Ipsum ea excepturi quam blanditiis temporibus. </p>
-                </span>
-                <span className="person">
-                    <h4>Person, Director</h4>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis error reprehenderit minima rerum id dolore ipsa quos totam consectetur, praesentium sequi optio. Maiores eius sit, vitae odit ut cum error.</p>
-                </span>
-                <span className="person">
-                    <h4>Person, Director</h4>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, quod ducimus! Vero exercitationem quibusdam nobis alias! Iste aliquid in tempora vel cum dolorem natus asperiores repellendus blanditiis excepturi, id quos!</p>
+
                 </span>
             </div>
+               
         </div>
     )
 }
