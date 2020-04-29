d3c322b302bcc2cb6e0d455931c25b46914cecc3
Devan Skeem <devanskeem@gmail.com>
2020-04-29 11:38:41 -0500

    updated home page introduction

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -10,7 +10,7 @@ function Home() {
                 <div className="content-container">
                     <div className="home-text">
                         <p className='intro'>
-                            Hooper Irrigation Company is a non-profit irrigation company providing Hooper City with secondary irrigation water. This website is intended to help you find the information you are looking for regarding your irrigation water.
+                            Hooper Irrigation Company is a non-profit irrigation company providing Hooper City and Taylor with secondary irrigation water and flood irrigation water to specific areas in western Weber and Davis Counties. This website is intended to help you find the information you are looking for regarding your irrigation water.
                         </p>
                         <div className="contact">
                             <h2 id='contact-us'>
