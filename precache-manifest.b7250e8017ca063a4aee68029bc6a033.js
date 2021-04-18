86a4048684eec3d8370ba4eff37cc245549b029c
Devan Skeem <devanskeem@gmail.com>
2021-04-18 16:04:45 -0700

    Updates

---
+++ precache-manifest.b7250e8017ca063a4aee68029bc6a033.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b6493ccf5556303dda7427bf40abb5d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4cfeb53012007eb3ff11",
+    "url": "/static/css/main.481643b7.chunk.css"
+  },
+  {
+    "revision": "c5383f1e678975f3506c",
+    "url": "/static/js/2.64ce052d.chunk.js"
+  },
+  {
+    "revision": "dbe5ca7a82c728776bb536c3d2d64135",
+    "url": "/static/js/2.64ce052d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4cfeb53012007eb3ff11",
+    "url": "/static/js/main.a5f87399.chunk.js"
+  },
+  {
+    "revision": "9822300cbc8a2d524f72",
+    "url": "/static/js/runtime-main.35e1b92c.js"
+  },
+  {
+    "revision": "6f96a2e7762b121844471f56cd29eb9a",
+    "url": "/static/media/Beus Mitchell Master Half Turns 2021.6f96a2e7.pdf"
+  },
+  {
+    "revision": "7cfb587702331905385f3f71277941e2",
+    "url": "/static/media/Buck McLean Master Half Turns 2021.7cfb5877.pdf"
+  },
+  {
+    "revision": "24964ab056931befba1fdcd9b82e5408",
+    "url": "/static/media/Cristelli Penman Master Half Turns 2021.24964ab0.pdf"
+  },
+  {
+    "revision": "68a775f07c3777c10c2cbe735066304e",
+    "url": "/static/media/Dance Calderwood Master Half Turns 2021.68a775f0.pdf"
+  },
+  {
+    "revision": "99c14cb6905c1036fc0fe508951774be",
+    "url": "/static/media/Gibson East Master Half Turns 2021.99c14cb6.pdf"
+  },
+  {
+    "revision": "6ba57e9af5c5ab0823d692137a33af65",
+    "url": "/static/media/Hooper Irr Co Std Details.6ba57e9a.pdf"
+  },
+  {
+    "revision": "538771e8a13115f31a6f3e00357f56b1",
+    "url": "/static/media/Hooper Irrigation Bylaws.538771e8.pdf"
+  },
+  {
+    "revision": "4fab6197528b52577ca723eb40750b8e",
+    "url": "/static/media/Hooper Main Zinie Master Half Turns 2021.4fab6197.pdf"
+  },
+  {
+    "revision": "c7011f5c29973d238e1451933260b609",
+    "url": "/static/media/McFarland Heslop Master Half Turns 2021.c7011f5c.pdf"
+  },
+  {
+    "revision": "65cc50db8ac6bd689db8a7ea473537f4",
+    "url": "/static/media/Muskrat 1 & 3 Master Half Turns 2021.65cc50db.pdf"
+  },
+  {
+    "revision": "9cd92fac4e9a05c645402c136a8dee8f",
+    "url": "/static/media/Muskrat 4 Fowers Master Half Turns 2021.9cd92fac.pdf"
+  },
+  {
+    "revision": "4c33ee8135916a4a14f0522e0c7fa7e4",
+    "url": "/static/media/N Simpson W Simpson Master Half Turns 2021.4c33ee81.pdf"
+  },
+  {
+    "revision": "d3f9af5c541c79c03fec2e03050111ac",
+    "url": "/static/media/Naisbitt Cox Master Half Turns 2021.d3f9af5c.pdf"
+  },
+  {
+    "revision": "862a6279af9c1de72c9e819f59787169",
+    "url": "/static/media/Service Agreement.862a6279.pdf"
+  },
+  {
+    "revision": "0ccc37232747faa50919cbace1d30d99",
+    "url": "/static/media/South 1 & 6 Master Half Turns 2021.0ccc3723.pdf"
+  },
+  {
+    "revision": "95562d96a695050ffd1df1ca4bf2face",
+    "url": "/static/media/South 11 & 12 Master Half Turns 2021.95562d96.pdf"
+  },
+  {
+    "revision": "27eee1d9c4bbd2d9d7ae8262fe0f2959",
+    "url": "/static/media/South 2 & 3 Master Half Turns 2021.27eee1d9.pdf"
+  },
+  {
+    "revision": "fdb04ef8d63c8069094992c75bebdf26",
+    "url": "/static/media/South 4 & 5 Master Half Turns 2021.fdb04ef8.pdf"
+  },
+  {
+    "revision": "f4e3f234ffe5f712ec871a0f53a99566",
+    "url": "/static/media/South 7 & 8 Master Half Turns 2021.f4e3f234.pdf"
+  },
+  {
+    "revision": "c128a460d5869def2832d01b9a028ef8",
+    "url": "/static/media/South 9 & 10 Master Half Turns 2021.c128a460.pdf"
+  },
+  {
+    "revision": "a2154328658394c0797502255dc3b34c",
+    "url": "/static/media/Taylor Half 1 Master Half Turns 2021.a2154328.pdf"
+  },
+  {
+    "revision": "bf89cdac0789862df0c96e1e642060c5",
+    "url": "/static/media/Taylor Half 2 Master Half Turns 2021.bf89cdac.pdf"
+  },
+  {
+    "revision": "e824e442ac447cdc71a3acda3385aea5",
+    "url": "/static/media/Taylor Half 3 Master Half Turns 2021.e824e442.pdf"
+  },
+  {
+    "revision": "3a96c679a55a5c95da04723a72079416",
+    "url": "/static/media/WW8 Surrage Master Half Turns 2021.3a96c679.pdf"
+  },
+  {
+    "revision": "17952c68f8fdacd6329ceeee5c742a4b",
+    "url": "/static/media/West Moore Master Half Turns 2021.17952c68.pdf"
+  },
+  {
+    "revision": "c14278ccc5562f274da1369171ea9a0a",
+    "url": "/static/media/Widdison Hull Master Half Turns 2021.c14278cc.pdf"
+  },
+  {
+    "revision": "2f554a569eba11ccf6bc9eb547d89b82",
+    "url": "/static/media/hic-logo2.2f554a56.png"
+  },
+  {
+    "revision": "565da2a9483ebcc7bf5ec4d1882a167b",
+    "url": "/static/media/hooper-city.565da2a9.jpg"
+  },
+  {
+    "revision": "48dff49511d36a7e1f9dc6b70f4f1e6c",
+    "url": "/static/media/sprinklers.48dff495.jpeg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.43f9763f27e47909813cce5a17e99034.js"
+  "/precache-manifest.b7250e8017ca063a4aee68029bc6a033.js"
 );
 
 self.addEventListener('message', (event) => {
