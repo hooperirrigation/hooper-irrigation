97ef2f815a4c38f208dd7e6c456609091444b687
Devan <devanskeem@gmail.com>
2023-05-13 17:23:48 -0700

    made logo a link to homepage

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import './App.css';
 import { HashRouter } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import routes from './routes'
 import Footer from './components/Footer/Footer'
 import Nav from './components/Nav/Nav'
 import logo from './Assets/hic-logo2.png'
+
 function App() {
   return (
       <div className="App">
         <div className="overlay">
         <HashRouter basename='/'>
           <div className="header">
-            <img className='logo' src={logo} alt="" />
+            <Link to='/'>
+              <img className='logo' src={logo} alt="" />
+            </Link>
             <Nav />
           </div>
           {routes}
